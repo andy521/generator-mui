8b711f9e5f21648cfd7eb952a3a225d5913b13fa
怀天 <huaitian.ykb@taobao.com>
2013-09-13 18:20:23 +0800

    '添加封面'

--- app/index.js
+++ app/index.js
@@ -82,7 +82,7 @@ MuiGenerator.prototype.app = function app() {
 
     this.template('example/src/index.js', path.join(src, 'index.js'));
     this.template('example/src/init.js', path.join(src, 'init.js'));
-    this.copy('example/src/style.css', path.join(src, 'style.' + this.style));
+    this.copy('example/src/index.css', path.join(src, 'index.' + this.style));
     this.copy('example/templates/index.html', path.join(templates, 'index.html'));
     this.copy('example/templates/index.php', path.join(templates, 'index.php'));
     this.copy('example/templates/index.vm', path.join(templates, 'index.vm'));
